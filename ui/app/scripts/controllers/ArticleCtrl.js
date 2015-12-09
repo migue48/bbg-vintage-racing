08dc497868cfcd557d090429fac9ae5f44cf4e32
Miguel Osorio <migue48@gmail.com>
2015-12-08 22:04:57 -0800

    Create ServiceTemplate for AngularJs
    Create ServiceTemplate that can be reused for other controllers.

--- ui/app/scripts/controllers/ArticleCtrl.js
+++ ui/app/scripts/controllers/ArticleCtrl.js
@@ -1,35 +1,179 @@
 'use strict';
 
-(function() {
-  var ArticleCtrl;
 
-  ArticleCtrl = (function() {
-    function ArticleCtrl($log, ArticleService) {
-      this.$log = $log;
-      this.ArticleService = ArticleService;
-      this.$log.debug("constructing ArticleController");
-      this.articles = [];
-      this.getAllArticles();
-    }
+var UpdateArticleCtrl = (function() {
+  function UpdateArticleCtrl($log, $location, $stateParams, ArticleService) {
+    this.$log = $log;
+    this.$location = $location;
+    this.$stateParams = $stateParams;
+    this.ArticleService = ArticleService;
+    this.$log.debug("constructing UpdateArticleController");
+    this.article = {};
+    this.findArticle();
+  }
 
-    ArticleCtrl.prototype.getAllArticles = function() {
-      this.$log.debug("getAllArticles()");
-      return this.ArticleService.listArticles().then((function(_this) {
-        return function(data) {
-          _this.$log.debug("Promise returned " + data.length + " Articles");
-          return _this.articles = data;
-        };
-      })(this), (function(_this) {
-        return function(error) {
-          return _this.$log.error("Unable to get Articles: " + error);
-        };
-      })(this));
-    };
+  UpdateArticleCtrl.prototype.updateArticle = function() {
+    this.$log.debug("updateArticle()");
+    this.article.active = true;
+    return this.ArticleService.update(this.$stateParams.id, this.article).then((function(_this) {
+      return function(data) {
+        _this.$log.debug("Promise returned " + data + " Article");
+        return _this.$location.path("/admin/articles");
+      };
+    })(this), (function(_this) {
+      return function(error) {
+        return _this.$log.error("Unable to update Article: " + error);
+      };
+    })(this));
+  };
 
-    return ArticleCtrl;
+  UpdateArticleCtrl.prototype.findArticle = function() {
+    var id;
+    id = this.$stateParams.id;
+    this.$log.debug("Finding article id: " + id);
+    // Update this to use backend find article API
+    return this.ArticleService.list().then((function(_this) {
+      return function(data) {
+        _this.$log.debug("Promise returned " + data.length + " Articles");
+        _this.$log.debug(data);
+        _this.article = (data.filter(function(article) {
+          return article.id === id;
+        }))[0];
+        return _this.$log.debug(_this.article);
+      };
+    })(this), (function(_this) {
+      return function(error) {
+        return _this.$log.error("Unable to get Articles: " + error);
+      };
+    })(this));
+  };
 
-  })();
+  return UpdateArticleCtrl;
 
-  controllersModule.controller('ArticleCtrl', ['$log', 'ArticleService', ArticleCtrl]);
+})();
 
-}).call(this);
+controllersModule.controller('UpdateArticleCtrl', ['$log', '$location', '$stateParams', 'ArticleService', UpdateArticleCtrl]);
+
+
+var ArticleShowCtrl = (function() {
+  function ArticleShowCtrl($log, $stateParams, ArticleService) {
+    this.$log = $log;
+    this.$stateParams = $stateParams;
+    this.ArticleService = ArticleService;
+    this.article = {};
+    this.getArticle(this.$stateParams.id);
+  }
+
+  ArticleShowCtrl.prototype.getArticle = function(id) {
+    return this.ArticleService.show(id).then((function(_this) {
+      return function(data) {
+        return _this.article = data.article;
+      };
+    })(this), (function(_this) {
+      return function(error) {
+        return _this.$log.error("Unable to get article: " + error);
+      };
+    })(this));
+  };
+  return ArticleShowCtrl;
+})();
+
+controllersModule.controller('ArticleShowCtrl', ['$log', '$stateParams', 'ArticleService', ArticleShowCtrl]);
+
+
+
+var ArticleDeleteCtrl = (function() {
+  function ArticleDeleteCtrl($log, $stateParams, $location, ArticleService) {
+    this.$log = $log;
+    this.ArticleService = ArticleService;
+    this.$stateParams = $stateParams;
+    this.$location = $location;
+    this.deleteArticle();
+  }
+
+  ArticleDeleteCtrl.prototype.deleteArticle = function() {
+    return this.ArticleService.destroy(this.$stateParams.id).then((function(_this) {
+      return function(data) {
+        _this.$log.debug("Promise returned " + data.length + " Articles");
+        return _this.$location.path("/admin/articles");
+      };
+    })(this), (function(_this) {
+      return function(error) {
+        return _this.$log.error("Unable to delete Article: " + error);
+      };
+    })(this));
+  };
+
+  return ArticleDeleteCtrl;
+
+})();
+
+controllersModule.controller('ArticleDeleteCtrl', ['$log', '$stateParams', '$location', 'ArticleService', ArticleDeleteCtrl]);
+
+
+
+
+var CreateArticleCtrl = (function() {
+  function CreateArticleCtrl($rootScope, $log, $location, ArticleService) {
+    this.$log = $log;
+    this.$rootScope = $rootScope;
+    this.$location = $location;
+    this.ArticleService = ArticleService;
+    this.$log.debug("constructing CreateArticleController");
+    this.article = {};
+  }
+
+  CreateArticleCtrl.prototype.createArticle = function() {
+    this.$log.debug("createArticle()");
+    this.article.active = true;
+    this.article.language = "en";
+    this.article.userId = this.$rootScope.user.userID;
+
+    return this.ArticleService.create(this.article).then((function(_this) {
+      return function(data) {
+        _this.$log.debug("Promise returned " + data + " Article");
+        return _this.$location.path('/admin/articles');
+      };
+    })(this), (function(_this) {
+      return function(error) {
+        return _this.$log.error("Unable to create Article: " + error);
+      };
+    })(this));
+  };
+
+  return CreateArticleCtrl;
+
+})();
+
+controllersModule.controller('CreateArticleCtrl', ['$rootScope', '$log', '$location', 'ArticleService', CreateArticleCtrl]);
+
+
+
+var ArticleCtrl =  (function() {
+  function ArticleCtrl($log, ArticleService) {
+    this.$log = $log;
+    this.ArticleService = ArticleService;
+    this.$log.debug("constructing ArticleController");
+    this.articles = [];
+    this.getAllArticles();
+  }
+
+  ArticleCtrl.prototype.getAllArticles = function() {
+    this.$log.debug("getAllArticles()");
+    return this.ArticleService.list().then((function(_this) {
+      return function(data) {
+        _this.$log.debug("Promise returned " + data.length + " Articles");
+        return _this.articles = data;
+      };
+    })(this), (function(_this) {
+      return function(error) {
+        return _this.$log.error("Unable to get Articles: " + error);
+      };
+    })(this));
+  };
+
+  return ArticleCtrl;
+})();
+
+controllersModule.controller('ArticleCtrl', ['$log', 'ArticleService', ArticleCtrl]);
+
---
+++ ui/app/scripts/services/ServiceTemplate.js
@@ -0,0 +1,100 @@
+'use strict';
+
+function ServiceTemplate(nameUrl) {
+  var Service;
+
+  Service.headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  };
+
+  Service.defaultConfig = {
+    headers: Service.headers
+  };
+
+  function Service($log, $http, $q) {
+    this.$log = $log;
+    this.$http = $http;
+    this.$q = $q;
+  }
+
+  Service.prototype.list = function() {
+    var deferred;
+    this.$log.debug('list()');
+    deferred = this.$q.defer();
+    this.$http.get(nameUrl).success((function(_this) {
+      return function(data, status, headers) {
+        return deferred.resolve(data);
+      };
+    })(this)).error((function(_this) {
+      return function(data, status, headers) {
+        _this.$log.error('Failed to get list - status ' + status);
+        return deferred.reject(data);
+      };
+    })(this));
+    return deferred.promise;
+  };
+
+  Service.prototype.show = function(id) {
+    var deferred;
+    deferred = this.$q.defer();
+    this.$http.get(nameUrl + '/' + id).success((function(_this) {
+      return function(data, status, headers) {
+        return deferred.resolve(data);
+      };
+    })(this)).error((function(_this) {
+       return function(data, status, headers) {
+         return deferred.reject(data);
+       };
+    })(this));;
+    return deferred.promise;
+  };
+
+  Service.prototype.create = function(_obj) {
+    var deferred;
+    this.$log.debug('create ' + (angular.toJson(_obj, true)));
+    deferred = this.$q.defer();
+    this.$http.post(nameUrl, _obj).success((function(_this) {
+      return function(data, status, headers) {
+        return deferred.resolve(data);
+      };
+    })(this)).error((function(_this) {
+      return function(data, status, headers) {
+        _this.$log.error('Failed to create object - status ' + status);
+        return deferred.reject(data);
+      };
+    })(this));
+    return deferred.promise;
+  };
+
+  Service.prototype.destroy = function(id) {
+    var deferred;
+    deferred = this.$q.defer();
+    this.$http.delete(nameUrl + '/delete/' + id).success((function(_this) {
+      return function(data, status, headers) {
+        return deferred.resolve(data);
+      };
+    })(this));
+    return deferred.promise;
+  };
+
+  Service.prototype.update = function(id, _obj) {
+    var deferred;
+    this.$log.debug('update ' + (angular.toJson(_obj, true)));
+    deferred = this.$q.defer();
+    this.$http.put(nameUrl + '/' + id, _obj).success((function(_this) {
+      return function(data, status, headers) {
+        return deferred.resolve(data);
+      };
+    })(this)).error((function(_this) {
+      return function(data, status, header) {
+        _this.$log.error('Failed to update object - status ' + status);
+        return deferred.reject(data);
+      };
+    })(this));
+    return deferred.promise;
+  };
+
+  return Service;
+
+};
