4d393718462716c7e8e7df7cb5a674d74e19c6b9
Miguel Osorio <migue48@gmail.com>
2016-01-11 23:36:59 -0800

    Article Preview Directive
    This is a baseline implementation of the article preview directive.
    This implementation generates the preview using the first two
    paragraphs of the article.

--- ui/app/scripts/directives/ArticlePreview.js
+++ ui/app/scripts/directives/ArticlePreview.js
@@ -2,6 +2,11 @@
 
 var articlePreview = angular.module('articlePreview', []);
 
+/**
+ * The articlePreview directive is used to generate article preview views.
+ * Currently, the preview is based on the first two paragraphs in the
+ * article.
+ */
 articlePreview.directive('articlePreview', function() {
   return {
     // The restrict option can be se to:
@@ -11,15 +16,50 @@ articlePreview.directive('articlePreview', function() {
     // These options can be combined e.g. 'AEC'
     restrict: 'A',
     replace: true,
-    templateUrl: '/partials/articles/preview.html',
-    scope: {
-      article: '=data',
-      previmg: '@',
-    },
-    link: function(scope, elem, attr, ctrl, transclude) {
-      // populate title, find cover image and calculate text preview.
-      scope.title = 'This is the title';
-      scope.preview = 'This is the text preview';
+    link: function (scope, element, attrs) {
+      var rawData;
+      var articleId;
+
+      /**
+       * Find the N-th ending paragraph index of an input HTML string. The
+       *     function searches for '</p>' tags n times.
+       * @param {html} String containing HTML text.
+       * @param {i} Start index for search operation.
+       * @param {n} N-th order.
+       * @return index of N-th end of paragraph, otherwise -1.
+       */
+      function FindEndOfNthParagraph(html, i, n) {
+        var idx = html.indexOf('</p>', i);
+        if (n == 0)
+          return idx;
+
+        return FindEndOfNthParagraph(html, idx + 1, n - 1);
+      };
+
+      /**
+       * Generate the article preview. The current implementation generates
+       * the preview using the first two paragraphs in the article.
+       */
+      function GenPreview() {
+        var idx = FindEndOfNthParagraph(rawData, 0, 1);
+        var prevHtml;
+        if (idx < 0) {
+          // Not enough paragraphs to generate preview.
+          prevHtml = rawData;
+        } else {
+          prevHtml  = rawData.substring(0, idx) + '</p>';
+          prevHtml += '<hr>';
+          prevHtml += '<a href="/#/news/'+ articleId +'" class="bbg_red bbg_fnt_md pull-right">Read More <i class="fa fa-plus"></i></a>';
+        }
+        element.html(prevHtml);
+      };
+
+      scope.$watch(attrs.articlePreview, function(value) {
+        rawData = value;
+        articleId = attrs.id;
+        GenPreview();
+      });
+
     }
   };
 });
